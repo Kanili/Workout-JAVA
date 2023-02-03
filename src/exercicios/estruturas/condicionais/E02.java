2fcb2fd4b757ea95b1507babe737daef22ab7d94
Kanili <zatch73@duck.com>
2023-02-03 10:24:05 -0300

    exercicio 2 resolvido

--- src/exercicios/estruturas/condicionais/E02.java
+++ src/exercicios/estruturas/condicionais/E02.java
@@ -1,4 +1,18 @@
 package exercicios.estruturas.condicionais;
 
 public class E02 {
+    public static void main(String[] args) {
+        // Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa. Caso sexo seja “F” e
+        //estado civil seja “CASADA”, solicitar o tempo de casada (anos).
+
+        String nome = "Marisa";
+        String sexo = "F";
+        String estadoCivil = "CASADA";
+
+        if(sexo == "F" && estadoCivil == "CASADA"){
+            System.out.println("SOLICITAÇÃO DE QUANTDOS ANOS DE CASADA");
+        }else {
+            System.out.println("Tudo certo!");
+        }
+    }
 }
